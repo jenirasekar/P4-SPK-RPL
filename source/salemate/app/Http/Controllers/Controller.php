51c148beb8bdce6dd46a873f9ce38d6328f4850a
Jenira Sekar Az-zahra <jenirasekar@gmail.com>
2023-12-10 12:39:48 +0700

    chore: layouting

--- source/salemate/app/Http/Controllers/Controller.php
+++ source/salemate/app/Http/Controllers/Controller.php
@@ -5,8 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Spatie\FlareClient\View;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function index()
+    {
+        return View('layouts.master');
+    }
 }
---
+++ source/salemate/resources/views/layouts/master.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SaleMate</title>
+
+    {{-- Bootstrap CSS --}}
+    <link rel="stylesheet" href="{!! url('asset/css/bootstrap.min.css') !!}">
+
+    {{-- Google fonts --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@200;400;600;700;900&family=Montserrat:ital,wght@0,300;0,400;0,500;0,700;1,800&display=swap"
+        rel="stylesheet">
+
+    {{-- my CSS --}}
+    <link rel="stylesheet" href="{!! url('asset/css/style.css') !!}">
+</head>
+
+<body>
+    <div class="wrapper">
+        {{-- Sidebar --}}
+        @include('layouts.sidebar')
+
+        {{-- Content --}}
+        <section class="content">
+            <div class="container-fluid d-flex">
+                @yield('content')
+            </div>
+        </section>
+    </div>
+
+    {{-- Bootstrap JS --}}
+    <script src="{!! url('asset/js/bootstrap.bundle.min.js') !!}"></script>
+</body>
+
+</html>
