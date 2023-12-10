e29a1d92351dd812177eb3923257eee77e55bd26
Jenira Sekar Az-zahra <jenirasekar@gmail.com>
2023-12-10 21:43:08 +0700

    feat: landing page

--- source/salemate/resources/views/dashboard/index.blade.php
+++ source/salemate/resources/views/dashboard/index.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1>hai</h1>
+    <div class="row mt-5">
+        <div class="col-10">
+            <h1>Home page</h1>
+            <p>Silakan login untuk melihat data terbatas</p>
+        </div>
+
+        <div class="col-2">
+            <a href="/" class="login-btn btn btn-danger btn-lg">
+                <img src="{!! url('assets/img/icons/person-icon.svg') !!}" alt="person">
+                Login
+            </a>
+        </div>
+    </div>
 @endsection
