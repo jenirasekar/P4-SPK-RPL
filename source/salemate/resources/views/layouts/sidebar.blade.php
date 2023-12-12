ea08e2cd1b2615a988624076775f0f114355407a
Jenira Sekar Az-zahra <jenirasekar@gmail.com>
2023-12-12 22:36:44 +0700

    style: fix posisi nav items

--- source/salemate/resources/views/layouts/sidebar.blade.php
+++ source/salemate/resources/views/layouts/sidebar.blade.php
@@ -9,74 +9,47 @@
     {{-- Sidemenu --}}
     <nav>
         <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
-            <li class="nav-item">
-                <a href="#" class="nav-link py-3 border-none" aria-current="page" title="Home"
-                    data-bs-toggle="tooltip" data-bs-placement="right">
-                    <svg class="bi" width="24" height="24" role="img" aria-label="Home">
-                        <use xlink:href="/" />
-                    </svg>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link py-3 border-none" aria-current="page" title="Home"
-                    data-bs-toggle="tooltip" data-bs-placement="right">
-                    <svg class="bi" width="24" height="24" role="img" aria-label="Home">
-                        <use xlink:href="/" />
-                    </svg>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link py-3 border-none" title="Dashboard" data-bs-toggle="tooltip"
-                    data-bs-placement="right">
-                    <img src="{!! url('assets/img/icons/dashboard-icon.svg') !!}" alt="Dashboard" width="20" height="20">
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link py-3 border-none" title="Produk" data-bs-toggle="tooltip"
-                    data-bs-placement="right">
-                    <img src="{!! url('assets/img/icons/box-icon.svg') !!}" alt="Produk" width="20" height="20">
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link py-3 border-none" title="Transaksi" data-bs-toggle="tooltip"
-                    data-bs-placement="right">
-                    <img src="{!! url('assets/img/icons/exchange-icon.svg') !!}" alt="Transaksi" width="20" height="20">
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link py-3 border-none" title="Laporan" data-bs-toggle="tooltip"
-                    data-bs-placement="right">
-                    <img src="{!! url('assets/img/icons/form-icon.svg') !!}" alt="Laporan" width="20" height="20">
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link py-3 border-none" title="User" data-bs-toggle="tooltip"
-                    data-bs-placement="right">
-                    <img src="{!! url('assets/img/icons/person-icon.svg') !!}" alt="User" width="20" height="20">
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link py-3 border-none" aria-current="page" title="Home"
-                    data-bs-toggle="tooltip" data-bs-placement="right">
-                    <svg class="bi" width="24" height="24" role="img" aria-label="Home">
-                        <use xlink:href="/" />
-                    </svg>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link py-3 border-none" aria-current="page" title="Home"
-                    data-bs-toggle="tooltip" data-bs-placement="right">
-                    <svg class="bi" width="24" height="24" role="img" aria-label="Home">
-                        <use xlink:href="/" />
-                    </svg>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link py-3 border-none" title="Logout" data-bs-toggle="tooltip"
-                    data-bs-placement="right">
-                    <img src="{!! url('assets/img/icons/logout-icon.svg') !!}" alt="Logout" width="20" height="20">
-                </a>
-            </li>
+            <div class="main-nav-icons">
+                <li class="nav-item">
+                    <a href="#" class="nav-link py-3 border-none" title="Dashboard" data-bs-toggle="tooltip"
+                        data-bs-placement="right">
+                        <img src="{!! url('assets/img/icons/dashboard-icon.svg') !!}" alt="Dashboard" width="20" height="20">
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link py-3 border-none" title="Produk" data-bs-toggle="tooltip"
+                        data-bs-placement="right">
+                        <img src="{!! url('assets/img/icons/box-icon.svg') !!}" alt="Produk" width="20" height="20">
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link py-3 border-none" title="Transaksi" data-bs-toggle="tooltip"
+                        data-bs-placement="right">
+                        <img src="{!! url('assets/img/icons/exchange-icon.svg') !!}" alt="Transaksi" width="20" height="20">
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link py-3 border-none" title="Laporan" data-bs-toggle="tooltip"
+                        data-bs-placement="right">
+                        <img src="{!! url('assets/img/icons/form-icon.svg') !!}" alt="Laporan" width="20" height="20">
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link py-3 border-none" title="User" data-bs-toggle="tooltip"
+                        data-bs-placement="right">
+                        <img src="{!! url('assets/img/icons/person-icon.svg') !!}" alt="User" width="20" height="20">
+                    </a>
+                </li>
+            </div>
+
+            <div class="bottom-nav-icon">
+                <li class="nav-item">
+                    <a href="#" class="nav-link py-3 border-none" title="Logout" data-bs-toggle="tooltip"
+                        data-bs-placement="right">
+                        <img src="{!! url('assets/img/icons/logout-icon.svg') !!}" alt="Logout" width="20" height="20">
+                    </a>
+                </li>
+            </div>
         </ul>
         {{-- ./sidemenu --}}
     </nav>
